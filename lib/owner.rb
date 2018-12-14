d7b1b471b597711bff73a34d52d5c7f3fd5a8d41
Matteo Muscas <thatman.j@gmail.com>
2018-12-14 13:55:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
-  # code goes here
+  attr_accessor
+  attr_reader :species
+
+  @@all_owners = []
+
+  def initialize(species)
+    @species = species
+    @@all_owners << self
+  end
+
+  def self.all
+    @@all_owners
+  end
+
+  def self.reset_all
+    @@all_owners.clear
+  end
+
+  def self.count
+    @@all_owners.size
+  end
+
 end
